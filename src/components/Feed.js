6149d9d3086cdb6437c4cbf302699c955fa3cd79
Ravi Jain <jravi248@gmail.com>
2022-04-07 19:52:08 +0530

    Add starting routes

---
+++ src/components/Feed.js
@@ -0,0 +1,9 @@
+function Feed() {
+    return (
+        <div className="feed">
+            Hello from Feed
+        </div>
+    )
+}
+
+export default Feed;
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+function Profile() {
+    return (
+        <div className="feed">
+            Hello from Profile
+        </div>
+    )
+}
+
+export default Profile;
