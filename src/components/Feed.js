c92aacb48a9c51334cd0523641224c0b20e9d29a
Akanksha Agarwal <agarwalakanksha027@gmail.com>
2022-04-13 02:34:31 +0530

    Add button

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,9 +2,19 @@ import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import dummyData from "./dummy.json";
 import FeedCard from "./FeedCard";
+import IconButton from "@mui/material/IconButton";
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
+import { useParams, useNavigate } from 'react-router-dom';
+import Tooltip from '@mui/material/Tooltip';
 
 export default function Feed({ user, homePage }) {
   const [elements, setElements] = useState([]);
+
+  const navigate = useNavigate();
+  const Redirect = () => {
+    navigate("/create");
+  }
 
   useEffect(() => {
 
@@ -27,21 +37,33 @@ export default function Feed({ user, homePage }) {
 
     // Fetch the articles
     fetch(endpoint, requestOptions)
-    .then(res => {
-      return res.json();
-    }).then(data => {
-      const localElements = data.map((article) => {
-        const date = new Date(article.createdAt);
-        const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-        return <FeedCard article_id={article._id} title={article.title} text={article.body} author={article.author.name} authorId={article.author._id} date={date.toLocaleDateString("en-US", dateOptions)} />;
+      .then(res => {
+        return res.json();
+      }).then(data => {
+        const localElements = data.map((article) => {
+          const date = new Date(article.createdAt);
+          const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+          return <FeedCard article_id={article._id} title={article.title} text={article.body} author={article.author.name} authorId={article.author._id} date={date.toLocaleDateString("en-US", dateOptions)} />;
+        });
+        setElements(localElements);
       });
-      setElements(localElements);
-    });
   }, []);
 
   return (
-    <div className={homePage ? "feed" : "user-feed"}>
-      {elements}
+    <div>
+      <div className={homePage ? "feed" : "user-feed"}>
+        {/* <Header /> */}
+        {elements}
+      </div>
+      <div style={{ position: "fixed", bottom: "1%", right: "1%" }}>
+        <Tooltip title="write" placement="bottom" >
+          <IconButton onClick={Redirect}>
+            <Fab color="secondary" aria-label="edit">
+              <EditIcon />
+            </Fab>
+          </IconButton>
+        </Tooltip>
+      </div>
     </div>
   );
 }
